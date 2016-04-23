be0aa0b15f48aa34da61f8a5f5ef77a9cee8f3e9
Gregory Kuruc <gkuruc@Gregorys-MacBook-Pro.local>
2016-04-23 19:22:25 -0400

    Fix words coming up out of order bug

--- backend/download_and_merge.sh
+++ backend/download_and_merge.sh
@@ -1,16 +1,15 @@
 rm final.mp4
 words=($1)
 echo ${words[*]}
+i=0
 for word in "${words[@]}"
 do
    first_letter="$(echo $word | head -c 1)"
-   err_code=`curl -O http://www.handspeak.com/word/$first_letter/$word.mp4`
-   if [ err_code -ne 0]
-      then
-         echo "This word does not exist..."
-   fi
+   err_code=`curl http://www.handspeak.com/word/$first_letter/$word.mp4 -o $i.mp4`
+   i=$((i+1))
 done
 
+ls
 i=0
 for orig_mp4 in *.mp4; do
    ffmpeg -i $orig_mp4 -r 20 $i.mpg
@@ -19,7 +18,9 @@ for orig_mp4 in *.mp4; do
 done
 
 cat *.mpg > final.mpg
-ffmpeg -i final.mpg -r 20 final.mp4
-ffmpeg -i final.mp4 -vf scale=920:640 final.mp4
+
+ffmpeg -i final.mpg -r 20 stage.mp4
+ffmpeg -i stage.mp4 -vf scale=920:640 final.mp4
 
 rm *.mpg   
+rm stage.mp4
