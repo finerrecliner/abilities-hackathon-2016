b5bfb959e36b2f1055cf7595f58d503accfdb7a5
Dave Fine <finerrecliner@gmail.com>
2016-04-23 10:42:59 -0400

    extension sends a POST to a server

--- chrome/ext.js
+++ chrome/ext.js
@@ -3,10 +3,14 @@ function menuOnClick(info, tab) {
 //  console.log("item " + info.menuItemId + " was clicked");
 //  console.log("info: " + JSON.stringify(info));
 //  console.log("tab: " + JSON.stringify(tab));
-  var searchstring = info.selectionText;
-  chrome.tabs.create({url: "http://maps.google.com/maps?q=" + searchstring})
+
+    var searchstring = info.selectionText;
+
+    xhttp = new XMLHttpRequest(); 
+    xhttp.open("POST", "http://ec2-52-32-12-200.us-west-2.compute.amazonaws.com/", true);
+    xhttp.send("hello world");
 }
 
-var id = chrome.contextMenus.create({"title": "Text2ASL", 
-                                     "contexts":["selection"], 
-                                     "onclick": menuOnClick});
+chrome.contextMenus.create({"title": "Text2ASL", 
+                            "contexts":["selection"], 
+                            "onclick": menuOnClick});
