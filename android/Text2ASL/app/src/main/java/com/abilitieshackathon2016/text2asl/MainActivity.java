04eb7978c46524b638206934162cdbd39e798a62
Dave Fine <finerrecliner@gmail.com>
2016-04-23 12:59:41 -0400

    created android project

---
+++ android/Text2ASL/app/src/main/java/com/abilitieshackathon2016/text2asl/MainActivity.java
@@ -0,0 +1,13 @@
+package com.abilitieshackathon2016.text2asl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
