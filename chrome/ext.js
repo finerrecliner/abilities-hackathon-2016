4274d5be21611332aa603fbcc6d7d5c08f5079ca
Dave Fine <finerrecliner@gmail.com>
2016-04-23 17:25:20 -0400

    updated descriptions and logos for chrome ext

--- chrome/ext.js
+++ chrome/ext.js
@@ -1,16 +1,18 @@
 
 function menuOnClick(info, tab) {
+    var searchstring = info.selectionText;
+
+    xhttp = new XMLHttpRequest(); 
+    xhttp.open("POST", "http://899eca95.ngrok.io/?words=" + info.selectionText, true);
+    xhttp.send();
+
+    console.log(info.selectionText);
 //  console.log("item " + info.menuItemId + " was clicked");
 //  console.log("info: " + JSON.stringify(info));
 //  console.log("tab: " + JSON.stringify(tab));
 
-    var searchstring = info.selectionText;
-
-    xhttp = new XMLHttpRequest(); 
-    xhttp.open("POST", "http://ec2-52-32-12-200.us-west-2.compute.amazonaws.com/", true);
-    xhttp.send("hello world");
 }
 
-chrome.contextMenus.create({"title": "Text2ASL", 
+chrome.contextMenus.create({"title": "Sign with Handy", 
                             "contexts":["selection"], 
                             "onclick": menuOnClick});
