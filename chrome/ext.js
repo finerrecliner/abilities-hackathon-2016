e4285f69a704b93fa4c2fce1245d4773b4506d56
Dave Fine <finerrecliner@gmail.com>
2016-04-23 10:22:32 -0400

    remove unneeded files. clicking on menu item opens a new tab with query

--- chrome/ext.js
+++ chrome/ext.js
@@ -1,14 +1,12 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-// A generic onclick callback function.
-function genericOnClick(info, tab) {
-  console.log("item " + info.menuItemId + " was clicked");
-  console.log("info: " + JSON.stringify(info));
-  console.log("tab: " + JSON.stringify(tab));
+function menuOnClick(info, tab) {
+//  console.log("item " + info.menuItemId + " was clicked");
+//  console.log("info: " + JSON.stringify(info));
+//  console.log("tab: " + JSON.stringify(tab));
+  var searchstring = info.selectionText;
+  chrome.tabs.create({url: "http://maps.google.com/maps?q=" + searchstring})
 }
 
 var id = chrome.contextMenus.create({"title": "Text2ASL", 
                                      "contexts":["selection"], 
-                                     "onclick": genericOnClick});
+                                     "onclick": menuOnClick});
